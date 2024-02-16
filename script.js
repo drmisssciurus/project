5cf66c5f0f365768859d92e0620e3df5b6b85267
drmisssciurus <ms.sciurus@gmail.com>
2024-02-16 19:03:38 +0200

    practis part 2

--- script.js
+++ script.js
@@ -10,13 +10,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('One of the last movies you watched?', ''),
-      b = prompt('How much would you rate it?', ''),
-      c = prompt('One of the last movies you watched?', ''),
-      d = prompt('How much would you rate it?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++){
+  const a = prompt('One of the last movies you watched?', ''),
+        b = prompt('How much would you rate it?', '');
+
+  if ( a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+    personalMovieDB.movies[a] = b;
+    console.log('Done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log('You have watched quite a few films');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('You are a classic viewer.');
+} else if (personalMovieDB.count >= 30) {
+  console.log('You are a movie buff.');
+} else {
+  console.log('An error occurred.');
+}
 
 console.log(personalMovieDB);
-
